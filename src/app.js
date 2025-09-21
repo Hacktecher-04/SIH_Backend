1de4cb4ff03acfdcf1c482644d6893467b4e6ce1
Mohit Vaishnav <157966300+mohitvaishnav56@users.noreply.github.com>
2025-09-21 17:52:36 +0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const server = http.createServer(app);
 app.use(cors({
     origin : [process.env.FRONTEND_URL,
       "http://localhost:3000",
+      "https://prismaroadmap.netlify.app"
     ],
     withCrendentials : true
 }));
