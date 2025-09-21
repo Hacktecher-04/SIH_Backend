92fdfb2f4c3668045593707330491436494cd037
Roshanbadgujar <hacktecher04@gmail.com>
2025-09-22 00:08:33 +0530

    resolve the auth problem

--- src/config/passport.js
+++ src/config/passport.js
@@ -21,7 +21,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: '/api/auth/google/callback',
+            callbackURL: process.env.GOOGLE_CALLBACK_URL || '/api/auth/google/callback',
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
@@ -53,7 +53,7 @@ passport.use(
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "/api/auth/github/callback"
+    callbackURL: process.env.GITHUB_CALLBACK_URL || "/api/auth/github/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
