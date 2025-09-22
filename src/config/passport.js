7c726776d76f9335215859cfe058dd5020504be7
Roshanbadgujar <hacktecher04@gmail.com>
2025-09-22 19:22:07 +0530

    updated login functionality

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const GitHubStrategy = require('passport-github2').Strategy;
 const User = require('../models/user.model');
+const userModel = require('../models/user.model');
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
@@ -26,6 +27,13 @@ passport.use(
         async (accessToken, refreshToken, profile, done) => {
             try {
                 let user = await User.findOne({ googleId: profile.id });
+                if (profile.emails[0].value) {
+                    const userData = await User.findOne({ email: profile.emails[0].value });
+                    if (userData) {
+                        userModel.findByIdAndUpdate(userData._id, { googleId: profile.id }, { new: true })
+                        return done(null, user);
+                    }
+                }
 
                 if (user) {
                     return done(null, user);
@@ -54,30 +62,37 @@ passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
     callbackURL: process.env.GITHUB_CALLBACK_URL || "/api/auth/github/callback"
-  },
-  async (accessToken, refreshToken, profile, done) => {
-    try {
-        let user = await User.findOne({ githubId: profile.id });
+},
+    async (accessToken, refreshToken, profile, done) => {
+        try {
+            let user = await User.findOne({ githubId: profile.id });
+            if (profile.emails[0].value) {
+                const userData = await User.findOne({ email: profile.emails[0].value });
+                if (userData) {
+                    userModel.findByIdAndUpdate(userData._id, { githubId: profile.id }, { new: true })
+                    return done(null, user);
+                }
+            }
 
-        if (user) {
-            return done(null, user);
-        } else {
-            user = await User.create({
-                githubId: profile.id,
-                fullName: {
-                    firstName: profile.displayName || profile.username,
-                    lastName: '',
-                },
-                username: profile.username,
-                email: profile.emails ? profile.emails[0].value : `${profile.username}@github.com`,
-                profilePicture: profile.photos[0].value,
-                role: 'user', // Default role
-            });
-            console.log(user);
-            return done(null, user);
+            if (user) {
+                return done(null, user);
+            } else {
+                user = await User.create({
+                    githubId: profile.id,
+                    fullName: {
+                        firstName: profile.displayName || profile.username,
+                        lastName: '',
+                    },
+                    username: profile.username,
+                    email: profile.emails ? profile.emails[0].value : `${profile.username}@github.com`,
+                    profilePicture: profile.photos[0].value,
+                    role: 'user', // Default role
+                });
+                console.log(user);
+                return done(null, user);
+            }
+        } catch (err) {
+            return done(err, null);
         }
-    } catch (err) {
-        return done(err, null);
     }
-  }
 ));
